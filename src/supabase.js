0e0b84a4dd3f63d3e6103d61baa94884ac705e73
capsaarang <43085582+capsaarang@users.noreply.github.com>
2026-06-28 17:59:48 -0400

    Update supabase.js

--- src/supabase.js
+++ src/supabase.js
@@ -1,14 +1,8 @@
 import { createClient } from "@supabase/supabase-js";
 
-const SUPABASE_URL = import.meta.env.VITE_SUPABASE_URL;
-const SUPABASE_ANON_KEY = import.meta.env.VITE_SUPABASE_ANON_KEY;
+const SUPABASE_URL = "https://tzqerlokaarezxwpkphz.supabase.co";
+const SUPABASE_ANON_KEY = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InR6cWVybG9rYWFyZXp4d3BrcGh6Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3ODI2NzE0OTksImV4cCI6MjA5ODI0NzQ5OX0.KtIl_RANv4TDQTAukiYsfHmT_LL06UKLSfvfWJfFRNk";
 
-export const supabase = createClient(SUPABASE_URL, SUPABASE_ANON_KEY, {
-  auth: {
-    autoRefreshToken: true,
-    persistSession: true,
-    detectSessionInUrl: true
-  }
-});
+export const supabase = createClient(SUPABASE_URL, SUPABASE_ANON_KEY);
 
 export const ADMIN_EMAIL = "sgovindaraj3@wisc.edu";
